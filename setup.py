156662b0be4d66e165927b8c12d4340df9b1f77b
rishu-13 <33250560+rishu-13@users.noreply.github.com>
2019-08-07 16:35:57 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ try:
 except IOError:
     long_description = ""
 setup(
-name="dsbox_corex",
-version="1.0.0",
-description="Return components/latent factors that explain the most multivariate mutual information in the data under Linear Gaussian model. For comparison, PCA returns components explaining the most variance in the data.",
-license="AGPL-3.0",
-author="Rob Brekelmans/Greg Ver Steeg",
-author_email="brekelma@usc.edu",
+name="fastmap",
+version="0.1",
+description="Fastmap algorithm for embedding graphs in Eucledean space",
+license="",
+author="Rishabh Pandey",
+author_email="rishabhp@usc.edu",
 keywords='d3m_primitive',
 #packages = ['corexcontinuous', 'corextext', ]
 packages=find_packages(),
-url='https://github.com/brekelma/dsbox_corex',
-download_url='https://github.com/brekelma/dsbox_corex',
+url='https://github.com/rishu-13/fastmap',
+download_url='https://github.com/rishu-13/fastmap',
 install_requires=[],
 long_description=long_description,
 include_package_data = True,
@@ -24,13 +24,6 @@ classifiers=[
 "Programming Language :: Python :: 3.5",
     ], 
 entry_points = {
-'d3m.primitives': [
-'feature_construction.corex_continuous.DSBOX = corex_continuous:CorexContinuous',
-'feature_construction.corex_text.DSBOX = corex_text:CorexText',
-'regression.echo_linear.DSBOX = echo_regressor:EchoLinearRegression',
-'feature_construction.echo_ib.DSBOX = echo_ib:EchoIB'
-#'feature_construction.corex_supervised.EchoIBReg = echo_sae:EchoRegression',        
-#'feature_construction.corex_supervised.EchoIBClf = echo_sae:EchoClassification'
-    ]
+'d3m.primitives': ['data_preprocessing.fast_map.DSBOX = fastmap_primitive:fastmap']
     }
 )
